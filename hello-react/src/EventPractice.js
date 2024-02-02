9fec5c61c394e213b62ac9abd39efc9f2bc77197
mywhzy <cmj2393a@naver.com>
2024-02-02 23:50:10 +0900

    feat: 4.2.3 임의 메서드 만들기 (화살표 함수 형태 ver.)

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -4,23 +4,28 @@ class EventPractice extends Component {
   state = {
     message: '',
   };
-  
-  constructor (props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this); // this가 EventPractice 컴포넌트 자신을 가리키기 위해서 메서드를 this와 binding!
-    this.handleClick = this.handleClick.bind(this); // 메서드 바인딩은 constructor에서 하는 것이 정석!
-  }
 
-  handleChange (e) {
+  /*
+  // 메서드 바인딩 (1) - 기본 방식 (constructor 사용)
+    constructor (props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this); // this가 EventPractice 컴포넌트 자신을 가리키기 위해서 메서드를 this와 binding!
+      this.handleClick = this.handleClick.bind(this); // 메서드 바인딩은 constructor에서 하는 것이 정석!
+    }
+    // 새 메서드 만들 때마다 constructor를 수정해줘야 하는 불편함 존재.
+   */
+
+  // 메서드 바인딩 (2) - Property Initializer Syntax 사용 (1)보다 훨씬 간단, 간편!
+  handleChange = (e) => {  // 바벨의 transform-class-properties 문법 사용해 화살표 함수 형태로 메서드 정의 (바인딩 효과)
     this.setState({
       message: e.target.value
     });
-  }
+  };
 
-  handleClick () {
+  handleClick = () => {
     alert(this.state.message);
     this.setState({ message: '' });
-  }
+  };
 
   render () {
     return (
