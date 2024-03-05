6a41f56fe6f2107b5a04144b0a6d420f668a50a9
mywhzy <cmj2393a@naver.com>
2024-03-05 19:17:23 +0900

    feat: 8.3 useReducer - 8.3.1 카운터 구현하기

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,18 +1,9 @@
-import { Info } from './Info';
-import { useState } from 'react';
+import { Counter } from './Counter';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
 
   return (
-    <div>
-      <button
-        onClick={() => setVisible(!visible)}>
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
+    <Counter />
   );
 };
 
