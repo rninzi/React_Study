31c1213425c4d3198ecb416b9a8d2fbbbee91a6e
mywhzy <cmj2393a@naver.com>
2024-05-31 23:39:59 +0900

    feat: 9.4.5 반응형 디자인 - 기본 ver.

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -5,6 +5,15 @@ const Box = styled.div`
   background: ${(props) => props.color || 'blue'};
   padding: 1rem;
   display: flex;
+  /* default는 가로 크기 1024px에 가운데 정렬, 가로 크기가 작아짐에 따라 크기를 줄이고 768px 미만이 되면 width를 꽉 채운다. */
+  width: 1024px;
+  margin: 0 auto;
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const Button = styled.button`
