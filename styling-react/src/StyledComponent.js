e76b8829533399bfd73ea29bab82d7753cce64b7
mywhzy <cmj2393a@naver.com>
2024-05-31 23:46:37 +0900

    feat: 9.4.5 반응형 디자인 - media query templates ver.

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -1,5 +1,21 @@
 import styled, { css } from 'styled-components';
 
+const sizes = {
+  desktop: 1024,
+  tablet: 768,
+};
+
+// 위에 있는 size 객체에 따라 자동 media 쿼리 함수를 만들기
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)};
+    }
+  `;
+
+  return acc;
+}, {});
+
 const Box = styled.div`
   /* props로 넣어준 값을 직접 전달해줄 수 있다 */
   background: ${(props) => props.color || 'blue'};
@@ -8,12 +24,8 @@ const Box = styled.div`
   /* default는 가로 크기 1024px에 가운데 정렬, 가로 크기가 작아짐에 따라 크기를 줄이고 768px 미만이 되면 width를 꽉 채운다. */
   width: 1024px;
   margin: 0 auto;
-  @media (max-width: 1024px) {
-    width: 768px;
-  }
-  @media (max-width: 768px) {
-    width: 100%;
-  }
+  ${media.desktop`width: 768px;`};
+  ${media.tablet`width: 100%;`};
 `;
 
 const Button = styled.button`
