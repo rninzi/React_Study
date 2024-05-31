eb1d2ba442230b4a9291d1d5cb1ef589ba8a6433
mywhzy <cmj2393a@naver.com>
2024-05-31 23:04:07 +0900

    feat: 9.3.2 Sass와 함께 사용하기

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import styles from './CSSModule.module.css';
+import styles from './CSSModule.module.scss';
 
 const cx = classNames.bind(styles); // 여러 개의 클래스 사용 or 조건부 클래스 설정 시 유용
 
