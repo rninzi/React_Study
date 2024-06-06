5c23b43962668ef76d587dcae3beb3f706a564a6
mywhzy <cmj2393a@naver.com>
2024-06-07 00:51:22 +0900

    feat: 9.3.3 CSS Module이 아닌 파일에서 CSS Module 사용하기

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,11 +1,12 @@
 import classNames from 'classnames/bind';
-import styles from './CSSModule.module.scss';
+import styles from './CSSModule.scss';
 
 const cx = classNames.bind(styles); // 여러 개의 클래스 사용 or 조건부 클래스 설정 시 유용
 
 const CSSModule = () => {
+  console.log(styles);
   return (
-    <div className={cx('wrapper', 'inverted')}>
+    <div className={cx('wrapper','inverted')}>
       안녕하세요, 저는 <span className="something">CSS Module!</span>
     </div>
   );
