369d60f1c57f3bde3a9a60e6b96383125439a27a
mywhzy <cmj2393a@naver.com>
2024-07-14 16:49:35 +0900

    feat: TodoListItem과 TodoList 만들기(UI)

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,12 @@
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
