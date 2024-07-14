12e8484af625a933ce6d1e29301904f768c7fbc0
mywhzy <cmj2393a@naver.com>
2024-07-14 17:40:40 +0900

    feat: App에서 todos 상태 사용하기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 import TodoList from './components/TodoList';
 
 function App() {
+  // 각 todo 항목의 고유 id, 내용, 완료 여부를 알려주는 값이 포함
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
