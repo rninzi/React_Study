13f7b3fe88ff62bc9b1995c0da49c7af72c04f31
mywhzy <cmj2393a@naver.com>
2024-10-16 01:53:31 +0900

    feat: 13.6.3 NotFound 페이지 만들기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Article from './pages/Article';
 import Articles from './pages/Articles';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 
 const App = () => {
@@ -17,6 +18,8 @@ const App = () => {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
+      {/* *는 아무 텍스트나 매칭한다는 뜻, 상단에 위치하는 라우트와 일치하는 라우트가 없는 경우 Not Found Page 출력 */}
     </Routes>
   );
 };
