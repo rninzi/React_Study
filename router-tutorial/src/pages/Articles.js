4c194209fbaa01b352f39998b13dd610f57a778e
mywhzy <cmj2393a@naver.com>
2024-10-16 01:46:53 +0900

    feat: 13.6.2 NavLink

--- router-tutorial/src/pages/Articles.js
+++ router-tutorial/src/pages/Articles.js
@@ -1,18 +1,30 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 const Articles = () => {
+  const activeStyle = {
+    color: 'green',
+    fontSize: 21,
+  };
+
   return (
     <div>
       <Outlet />
       <ul>
         <li>
-          <Link to="/articles/1">게시글 1</Link>
+          {/* NavLink: Link에서 사용하는 경로가 현재 라우트의 경로와 일치하는 경우 특정 스타일 or CSS 클래스를 적용하는 컴포넌트*/}
+          <NavLink to="/articles/1" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+            게시글 1
+          </NavLink>
         </li>
         <li>
-          <Link to="/articles/2">게시글 2</Link>
+          <NavLink to="/articles/2" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+            게시글 2
+          </NavLink>
         </li>
         <li>
-          <Link to="/articles/3">게시글 3</Link>
+          <NavLink to="/articles/3" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+            게시글 3
+          </NavLink>
         </li>
       </ul>
     </div>
