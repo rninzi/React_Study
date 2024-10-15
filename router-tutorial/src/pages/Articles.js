a01115549b41260d27813e46cf125d3b9aa07b4b
mywhzy <cmj2393a@naver.com>
2024-10-16 01:49:29 +0900

    feat: 13.6.2 NavLink - ArticleItem 컴포넌트 구현

--- router-tutorial/src/pages/Articles.js
+++ router-tutorial/src/pages/Articles.js
@@ -1,33 +1,34 @@
 import { NavLink, Outlet } from 'react-router-dom';
 
 const Articles = () => {
+  return (
+    <div>
+      <Outlet />
+      <ul>
+        <ArticleItem id={1} />
+        <ArticleItem id={2} />
+        <ArticleItem id={3} />
+      </ul>
+    </div>
+  );
+};
+
+const ArticleItem = ({ id }) => {
   const activeStyle = {
     color: 'green',
     fontSize: 21,
   };
 
   return (
-    <div>
-      <Outlet />
-      <ul>
-        <li>
-          {/* NavLink: Link에서 사용하는 경로가 현재 라우트의 경로와 일치하는 경우 특정 스타일 or CSS 클래스를 적용하는 컴포넌트*/}
-          <NavLink to="/articles/1" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            게시글 1
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/articles/2" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            게시글 2
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/articles/3" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            게시글 3
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    <li>
+      {/* NavLink: Link에서 사용하는 경로가 현재 라우트의 경로와 일치하는 경우 특정 스타일 or CSS 클래스를 적용하는 컴포넌트*/}
+      <NavLink
+        to={`/articles/${id}`}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
+        게시글 {id}
+      </NavLink>
+    </li>
   );
 };
 
