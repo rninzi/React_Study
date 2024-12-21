3e9ae9479e4f10ddbe5b722d54181a6c4c885ebb
mywhzy <cmj2393a@naver.com>
2024-12-22 00:15:42 +0900

    feat: newsapi 헤드라인 호출 국가 변경

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -6,8 +6,9 @@ const App = () => {
   const newsApiKey = process.env.REACT_APP_NEWS_API_KEY;
   const onClick = async () => {
     try {
+      // 현재 headline 호출 가능 국가가 us로 한정됨
       const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${newsApiKey}`,
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${newsApiKey}`,
       );
       setData(response.data);
     } catch (error) {
