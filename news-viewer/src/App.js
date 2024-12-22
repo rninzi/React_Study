dd075e82e1d3cd5a4fa7ce61a653316187efdd68
mywhzy <cmj2393a@naver.com>
2024-12-22 19:00:28 +0900

    feat: 14.4 뉴스 뷰어 UI 만들기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,34 +1,7 @@
-import { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const newsApiKey = process.env.REACT_APP_NEWS_API_KEY;
-  const onClick = async () => {
-    try {
-      // 현재 headline 호출 가능 국가가 us로 한정됨
-      const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${newsApiKey}`,
-      );
-      setData(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
