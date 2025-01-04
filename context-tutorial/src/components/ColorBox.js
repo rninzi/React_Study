088ff068ba1bd65c39cd5eb205b44245f84d15f6
mywhzy <cmj2393a@naver.com>
2025-01-04 23:31:00 +0900

    feat: 15.4.1 useContext Hook 사용하기

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,16 +1,14 @@
-import { ColorConsumer } from '../contexts/color';
+import { useContext } from 'react';
+import ColorContext from '../contexts/color';
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
+
   return (
-    <ColorConsumer>
-      {/* Function as a child or Render Props 구조 : 컴포넌트의 children 자리에 JSX를 반환하는 함수를 전달하는 것 */}
-      {({ state }) => (
-        <>
-          <div style={{ width: '64px', height: '64px', background: state.color }} />
-          <div style={{ width: '32px', height: '32px', background: state.subcolor }} />
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div style={{ width: '64px', height: '64px', background: state.color }} />
+      <div style={{ width: '32px', height: '32px', background: state.subcolor }} />
+    </>
   );
 };
 
