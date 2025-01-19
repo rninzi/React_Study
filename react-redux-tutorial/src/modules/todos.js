52318e5f28782a50cf7a815cc3052ffb9de50ecf
mywhzy <cmj2393a@naver.com>
2025-01-19 23:31:42 +0900

    feat: 17.6.2 immer

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
+import { produce } from 'immer';
 
 // 액션 타입 정의하기
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
@@ -40,27 +41,29 @@ const initialState = {
   ],
 };
 
+/* immer를 사용할 때 모든 업데이트 함수에 immer를 적용할 필요는 없다. 일반 JS로 처리하는 것이 더 간편할 땐 적용하지 않아도 된다. 
+  아래 코드에서 TOGGLE을 제외한 업데이트 함수는 immer을 사용하지 않는 것이 코드가 오히려 더 짧다. */
 const todos = handleActions(
   // 파라미터에 action을 넘겨 action.payload로 접근하는 것보다 객체 비구조화 할당 문법을 적용해 action 값의 payload 이름을 새로 설정해주면 더 명확한 의미가 전달된다.
   {
-    [CHANGE_INPUT]: (state, { payload: input }) => ({
-      ...state,
-      input: input,
-    }),
-    [INSERT]: (state, { payload: todo }) => ({
-      ...state,
-      todos: state.todos.concat(todo),
-    }),
-    [TOGGLE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo,
-      ),
-    }),
-    [REMOVE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.filter((todo) => todo.id !== id),
-    }),
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, (draft) => {
+        draft.input = input;
+      }),
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, (draft) => {
+        draft.todos.push(todo);
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const todo = draft.todos.find((todo) => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const index = draft.todos.findIndex((todo) => todo.id === id);
+        draft.todos.splice(index, 1);
+      }),
   },
   initialState,
 );
