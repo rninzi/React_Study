3665491c3e59b33231a65dc40b09946ee6184306
mywhzy <cmj2393a@naver.com>
2025-01-20 23:30:41 +0900

    feat: 17.7.2 useDispatch + useCallback으로 컴포넌트 성능 최적화하기

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -8,12 +9,10 @@ const CounterContainer = () => {
 
   // 액션 디스패치 (컴포넌트 내부에서 스토어의 내장 함수 dispatch를 사용 가능하게 함)
   const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter
-      number={number}
-      onIncrease={() => dispatch(increase())}
-      onDecrease={() => dispatch(decrease())}
-    />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
