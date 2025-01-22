2658826d2c9f73457c54429d7c8942412a04ec45
mywhzy <cmj2393a@naver.com>
2025-01-22 22:12:08 +0900

    feat: 17.7.5.2 TodosContainer에 useActions Hook 적용

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,7 +1,7 @@
-import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
 
 const TodosContainer = () => {
   // 비구조화 할당 문법 적용
@@ -9,15 +9,13 @@ const TodosContainer = () => {
     input: todos.input,
     todos: todos.todos,
   }));
-  const dispatch = useDispatch();
-  // 액션 종류가 많아 어떤 값이 액션 생성 함수의 파라미터로 사용돼야 하는지 일일이 명시해줘야하는 번거로움 존재
-  const onChangeInput = useCallback(
-    (input) => dispatch(changeInput(input)),
-    [dispatch],
+
+  // useActions Hook을 사용해 여러 개의 액션을 사용해도 코드 가독성 향상
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    [],
   );
-  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
   return (
     <Todos
       input={input}
