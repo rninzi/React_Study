0e93d8575c7e715394fc316e90ad77b9dd89178c
mywhzy <cmj2393a@naver.com>
2025-02-01 23:15:54 +0900

    feat: 18.1 작업 환경 준비

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
