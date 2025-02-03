c114a11594e1bec0359d04443ebc93772fa84b05
mywhzy <cmj2393a@naver.com>
2025-02-03 23:17:09 +0900

    chore: 미들웨어 관련 주석 추가

--- learn-redux-middleware/src/lib/loggerMiddleware.js
+++ learn-redux-middleware/src/lib/loggerMiddleware.js
@@ -1,8 +1,9 @@
+// store: 리덕스 스토어 인스턴스, action: 디스패치된 액션, next: 함수 형태(store.dispatch)와 비슷한 역할
 const loggerMiddleware = (store) => (next) => (action) => {
   console.group(action && action.type); // 액션 타입으로 log를 그룹화
   console.log('이전 상태', store.getState());
   console.log('액션', action);
-  next(action); // 다음 미들웨어 혹은 리듀서 전달
+  next(action); // 다음 미들웨어 혹은 리듀서 전달 (그다음 처리해야 할 미들웨어에게 액션을 넘겨주고, 만약 그 다음 미들웨어가 없다면 리듀서에게 액션을 넘겨준다.)
   console.log('다음 상태', store.getState()); // 업데이트된 상태
   console.groupEnd(); // 그룹 끝
 };
