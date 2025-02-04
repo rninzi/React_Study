6257f8733e1b0e2ae3e7656dbc59ba4d8a1a9a74
mywhzy <cmj2393a@naver.com>
2025-02-04 22:58:37 +0900

    feat: 18.3.1 redux-thunk 미들웨어 적용 및 Thunk 생성 함수 만들기

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -6,6 +6,20 @@ const DECREASE = 'counter/DECREASE';
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+// Thunk 생섬 함수 만들기
+// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 1000);
+};
+
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 1000);
+};
+
 const initialState = 0; // 상태가 꼭 객체일 필요 X, 숫자도 가능하다.
 
 const counter = handleActions(
