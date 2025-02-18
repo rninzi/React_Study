c39d1ebedb11cf4229a510b6d420e76579dec8c2
mywhzy <cmj2393a@naver.com>
2025-02-18 23:42:17 +0900

    feat: 18.3.2.2 리덕스 개발자 도구 적용해 액션 디스패치 상황 확인하기

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -10,10 +10,11 @@ import rootReducer, { rootSaga } from './modules';
 import { createLogger } from 'redux-logger';
 import { thunk } from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(logger, thunk, sagaMiddleware));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk, sagaMiddleware)));
 sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
