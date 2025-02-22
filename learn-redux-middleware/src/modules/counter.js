fe6fa19eb400724c825df2aeb1db3f8ea4623383
mywhzy <cmj2393a@naver.com>
2025-02-23 00:36:26 +0900

    feat: 18.3.2.5 알아 두면 유용한 기능들 - 사가 실행 주기 제한하기

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { delay, put, takeEvery, takeLatest, select } from 'redux-saga/effects';
+import { delay, put, takeLatest, select, throttle } from 'redux-saga/effects';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -26,8 +26,9 @@ function* decreaseSaga() {
 }
 
 export function* counterSaga() {
-  // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리해 줍니다.
-  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  // 첫 번째 파라미터: n초 * 1000
+  // throttle: 사가 실행 주기 제한 (사가가 n초에 단 한 번만 호출되도록 설정)
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
   // takeLatest는 기존에 진행 중이던 작업이 있다면 취소 처리하고 가장 마지막으로 실행된 작업만 수행합니다.
   yield takeLatest(DECREASE_ASYNC, decreaseSaga);
 }
