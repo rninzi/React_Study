1d73598d0ec87248b3ef630af952ddc023d471cc
mywhzy <cmj2393a@naver.com>
2025-02-24 23:26:26 +0900

    feat: 19.2.1 state를 사용한 코드 스플리팅

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,20 +1,29 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const onClick = () => {
-    // import() 함수 형태로 메서드 안에서 사용 시 빌드 폴더 내에서 파일을 따로 분리시켜서 저장한다. 또한 실제 함수가 필요한 지점에 파일을 불러와 함수를 사용할 수 있다.
-    // dynamic import
-    import('./notify').then((result) => result.default());
+class App extends Component {
+  state = {
+    SplitMe: null,
   };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-      </header>
-    </div>
-  );
+  handleClick = async () => {
+    const loadedModule = await import('./SplitMe');
+    this.setState({
+      SplitMe: loadedModule.default,
+    });
+  };
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>Hello React!</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
