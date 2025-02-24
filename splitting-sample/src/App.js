1e1b2272f4218688ee862f520255c8410b7b8948
mywhzy <cmj2393a@naver.com>
2025-02-24 23:20:09 +0900

    feat: 19.1 자바스크립트 함수 비동기 로딩

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -2,21 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const onClick = () => {
+    // import() 함수 형태로 메서드 안에서 사용 시 빌드 폴더 내에서 파일을 따로 분리시켜서 저장한다. 또한 실제 함수가 필요한 지점에 파일을 불러와 함수를 사용할 수 있다.
+    // dynamic import
+    import('./notify').then((result) => result.default());
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={onClick}>Hello React!</p>
       </header>
     </div>
   );
