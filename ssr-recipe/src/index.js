4a73c80a605c8c477553604856b3d2d9e5a82111
mywhzy <cmj2393a@naver.com>
2025-04-01 23:25:45 +0900

    fix: 리덕스에서 미들웨어 적용 에러 해결

--- ssr-recipe/src/index.js
+++ ssr-recipe/src/index.js
@@ -6,10 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer, applyMiddleware);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
