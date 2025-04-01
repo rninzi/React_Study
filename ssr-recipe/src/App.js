b9db8532b1491c140eccc30e5a5b84d1166d690c
mywhzy <cmj2393a@naver.com>
2025-04-01 23:26:08 +0900

    feat: 20.4.2 Users, UsersContainer 컴포넌트 준비하기

--- ssr-recipe/src/App.js
+++ ssr-recipe/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu';
 import RedPage from './pages/RedPage';
 import BluePage from './pages/BluePage';
+import UsersPage from './pages/UsersPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/red" Component={RedPage} />
         <Route path="/blue" Component={BluePage} />
+        <Route path="/users" Component={UsersPage} />
       </Routes>
     </div>
   );
