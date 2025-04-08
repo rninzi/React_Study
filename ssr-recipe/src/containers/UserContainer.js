7ad58f231e4d4656bcbc8dd473edf3455411ca88
mywhzy <cmj2393a@naver.com>
2025-04-08 23:43:13 +0900

    feat: 20.4.9 usePreloader Hook 만들어서 사용하기

--- ssr-recipe/src/containers/UserContainer.js
+++ ssr-recipe/src/containers/UserContainer.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import User from '../components/User';
-import { Preloader } from '../lib/PreloadContext';
+import { usePreloader } from '../lib/PreloadContext';
 import { getUser } from '../modules/users';
 import { useParams } from 'react-router-dom';
 
@@ -10,16 +10,14 @@ const UserContainer = () => {
   const user = useSelector((state) => state.users.user);
   const dispatch = useDispatch();
 
+  // 함수형 컴포넌트에선 usePreloader Hook을 사용하면 된다.
+  usePreloader(() => dispatch(getUser(id))); // 서버 사이드 렌더링을 할 때 API 호출하기
   useEffect(() => {
     if (user && user.id === parseInt(id, 0)) return; // 사용자 존재 & id 일치한다면 요청 X
     dispatch(getUser(id));
   }, [dispatch, id, user]); // id가 바뀔 때 새로 요청해야 함
 
-  // 컨테이너 유효성 검사 후 return null을 해야 하는 경우에 null 대신 Preloader 반환
-  // 이렇게 하면 서버 사이드 렌더링 과정에서 데이터가 없을 때 GET_USER 액션을 발생시킴
-  if (!user) {
-    return <Preloader resolve={() => dispatch(getUser(id))} />;
-  }
+  if (!user) return null;
   return <User user={user} />;
 };
 
