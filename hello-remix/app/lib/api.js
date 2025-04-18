aca7dccec5d92398a95ee7bd4af6893298783813
mywhzy <cmj2393a@naver.com>
2025-04-18 23:30:53 +0900

    feat: 20.6.2 API 함수 작성하기

--- hello-remix/app/lib/api.js
+++ hello-remix/app/lib/api.js
@@ -13,3 +13,21 @@ export async function getUser(id) {
   );
   return response.data;
 }
+
+export async function getStories() {
+  const response = await axios.get('http://localhost:4000/stories');
+  return response.data;
+}
+
+export async function getStory(id) {
+  const response = await axios.get(`http://localhost:4000/stories/${id}`);
+  return response.data;
+}
+
+export async function createStory({ title, body }) {
+  const response = await axios.post('http://localhost:4000/stories', {
+    title,
+    body,
+  });
+  return response.data;
+}
