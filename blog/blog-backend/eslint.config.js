8e877f5314ec7763be543fea38692cec86c81c8d
mywhzy <cmj2393a@naver.com>
2025-04-24 23:29:50 +0900

    feat: 21.2.3 ESLint와 Prettier 설정

---
+++ blog/blog-backend/eslint.config.js
@@ -0,0 +1,20 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const prettier = require('eslint-config-prettier');
+const { defineConfig } = require('eslint/config');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...prettier.rules,
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+]);
