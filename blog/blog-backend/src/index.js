756a0891a560dd0bcefd47a3a4694af818807e79
mywhzy <cmj2393a@naver.com>
2025-04-30 20:47:17 +0900

    feat: 21.3.2.2 async/await 사용하기

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -3,7 +3,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 // 미들웨어 함수를 애플리케이션에 등록 ctx(context): 웹 요청과 응답에 관한 정보 next: 현재 처리 중인 미들웨어의 다음 미들웨어
-app.use((ctx, next) => {
+app.use(async (ctx, next) => {
   console.log(ctx.url);
   console.log(1);
   if (ctx.query.authorized !== '1') {
@@ -11,9 +11,8 @@ app.use((ctx, next) => {
     return;
   }
   // next 함수를 호출하면 Promise를 반환 (다음에 처리해야 할 미들웨어가 끝나야 완료됨) -> Koa와 Express의 차이점
-  next().then(() => {
-    console.log('END');
-  });
+  await next();
+  console.log('END');
 });
 
 // 미들웨어는 app.use를 사용해 등록되는 순서대로 처리
