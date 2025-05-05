ab8fe4f36a392e54bb5e44fd1dc9cd2c9ab81afb
mywhzy <cmj2393a@naver.com>
2025-05-05 23:51:03 +0900

    feat: 21.5 koa-router 사용하기: 1. 기본 사용법

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,6 +1,19 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
+
+// 라우터 설정 - router.해당 라우트에서 사용 할 HTTP 메서드(이동 경로, 해당 라우트에 적용할 미들웨어 함수)
+router.get('/', (ctx) => {
+  ctx.body = '홈';
+});
+router.get('/about', (ctx) => {
+  ctx.body = '소개';
+});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
