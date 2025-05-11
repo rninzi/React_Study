3ee9c5845cf3d62dfe4621bf86cd04d4ee14b19d
mywhzy <cmj2393a@naver.com>
2025-05-11 22:17:41 +0900

    feat: 21.5.5.2 컨트롤러 파일 작성 - 포스트 조회, 생성, 제거, 수정

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,12 +1,18 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
 
+// 라우터 설정
 router.use('/api', api.routes()); // api 라우트 적용
+
+// 라우터 적용 전에 bodyParser 적용
+// bodyParser: HTTP 메소드의 Request Body에 JSON 형식으로 데이터를 넣어주면, 이를 파싱해 서버에서 사용할 수 있게 함
+app.use(bodyParser());
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
