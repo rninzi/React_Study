e9d7a081a42829a634992c7e9402edad36cce5e9
mywhzy <cmj2393a@naver.com>
2025-06-22 23:51:52 +0900

    feat: 23.4.3 토큰 검증하기

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,9 +1,13 @@
 import dotenv from 'dotenv';
-dotenv.config();
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+
+import api from './api/index.js';
+import jwtMiddleware from './lib/jwtMiddleware.js';
+
+dotenv.config();
 
 // 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
@@ -13,11 +17,9 @@ mongoose
   .then(() => {
     console.log('Connected to MongoDB');
   })
-  .catch((e) => {
+  .catch(e => {
     console.error(e);
   });
-
-import api from './api/index.js';
 
 const app = new Koa();
 const router = new Router();
@@ -28,6 +30,7 @@ router.use('/api', api.routes()); // api 라우트 적용
 // 라우터 적용 전에 bodyParser 적용
 // bodyParser: HTTP 메소드의 Request Body에 JSON 형식으로 데이터를 넣어주면, 이를 파싱해 서버에서 사용할 수 있게 함
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
