1e3579c9c787955385b93b4b158f85f3f45ff6b0
mywhzy <cmj2393a@naver.com>
2025-06-28 14:14:14 +0900

    feat: 23.5.5 포스트 수정 및 삭제 시 권한 확인하기

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -4,11 +4,23 @@ import Joi from 'joi';
 
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
   if (!ObjectId.isValid(id)) {
     ctx.status = 400; // Bad Request
     return;
+  }
+  try {
+    const post = await Post.findById(id);
+    // 포스트가 존재하지 않을 때
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
   }
   return next();
 };
@@ -91,17 +103,7 @@ export const list = async ctx => {
   GET /api/posts/:id 
 */
 export const read = async ctx => {
-  const { id } = ctx.params;
-  try {
-    const post = await Post.findById(id).exec();
-    if (!post) {
-      ctx.status = 404; // Not Found
-      return;
-    }
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
+  ctx.body = ctx.state.post;
 };
 
 /*
@@ -159,4 +161,15 @@ export const update = async ctx => {
   } catch (e) {
     ctx.throw(500, e);
   }
+};
+
+// id로 찾은 포스트가 로그인 중인 사용자가 작성한 포스트인지 확인
+export const checkOwnPost = (ctx, next) => {
+  const { user, post } = ctx.state;
+  // 사용자의 포스트가 아닌 경우 403에러 발생
+  if (post.user._id.toString() !== user._id) {
+    ctx.status = 403;
+    return;
+  }
+  return next();
 };
--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -30,6 +30,7 @@ router.use('/api', api.routes()); // api 라우트 적용
 // 라우터 적용 전에 bodyParser 적용
 // bodyParser: HTTP 메소드의 Request Body에 JSON 형식으로 데이터를 넣어주면, 이를 파싱해 서버에서 사용할 수 있게 함
 app.use(bodyParser());
+// app에 router 미들웨어를 적용하기 전에 jwtMiddleware를 적용해야 함
 app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
