ccd707c4d03a2005c2506574cc343aadbecd0848
mywhzy <cmj2393a@naver.com>
2025-07-16 22:59:57 +0900

    feat: 24.3.2.1 로그인 상태 보여주기

--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import Header from '../components/common/Header';
+import HeaderContainer from '../containers/common/HeaderContainer';
 
 const PostListPage = () => {
   return (
     <>
       <Outlet />
-      <Header />
+      <HeaderContainer />
       <div>안녕하세요.</div>
     </>
   );
