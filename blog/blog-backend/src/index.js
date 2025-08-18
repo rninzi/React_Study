63e95a4a11adceeefb07b2b9f75ba76aede45cd5
mywhzy <cmj2393a@naver.com>
2025-08-18 16:31:57 +0900

    feat: 27.4.2 koa-static으로 정적 파일 제공하기

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -3,6 +3,10 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
+import { fileURLToPath } from 'url';
 
 import api from './api/index.js';
 import jwtMiddleware from './lib/jwtMiddleware.js';
@@ -35,6 +39,19 @@ app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  // Not Found이고, 주소가 /api로 시작하지 않는 경우
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    // index.html 내용을 반환
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 // PORT가 지정되어 있지 않다면 4000을 사용
 const port = PORT || 4000;
